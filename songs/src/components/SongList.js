5b1655b931c681a232feb6f4b67be62c50b1387d
Mike VanSleen <mike.vansleen@gmail.com>
2019-07-05 15:38:42 -0600

    Song selection now displays current song selected

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,10 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends React.Component {
+  renderList = () => {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              onClick={() => this.props.selectSong(song)}
+              className="ui button primary"
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  };
+
   render() {
-    console.log(this.props);
-    return <div>Song List</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
@@ -14,4 +32,9 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(
+  mapStateToProps,
+  {
+    selectSong: selectSong
+  }
+)(SongList);
